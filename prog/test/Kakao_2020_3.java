7b4ad70e0a599cf734e3ddc18024f961e8371c06
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-09-07 22:55:23 +0900

    move files to subdirectory

--- prog/test/Kakao_2020_3.java
+++ prog/test/Kakao_2020_3.java
@@ -7,25 +7,25 @@ public class Kakao_2020_3 {
         String[] gems = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
         HashSet<String> gemsSet = new HashSet<>();
 //        ArrayList<int[]> answers = new ArrayList<>();
-        for (String g: gems)
+        for (String g : gems)
             gemsSet.add(g);
 
         int[] min = {0, Integer.MAX_VALUE};
         for (int i = 0; i < gems.length - gemsSet.size() + 1; i++) {
             ArrayList<String> gemCheck = new ArrayList<>();
-            int[] answer = {i+1,0};
+            int[] answer = {i + 1, 0};
             for (int j = i; j < gems.length; j++) {
                 if (!gemCheck.contains(gems[j]))
                     gemCheck.add(gems[j]);
-                if(gemCheck.size() == gemsSet.size()){
-                    answer[1] = j+1;
+                if (gemCheck.size() == gemsSet.size()) {
+                    answer[1] = j + 1;
                     break;
                 }
             }
-            if(answer[1] != 0) {
+            if (answer[1] != 0) {
                 int minDiff = min[1] - min[0];
                 int newDiff = answer[1] - answer[0];
-                if(minDiff > newDiff){
+                if (minDiff > newDiff) {
                     min[0] = answer[0];
                     min[1] = answer[1];
                 }
@@ -42,6 +42,6 @@ public class Kakao_2020_3 {
 //                min = arr;
 //        }
 
-        System.out.println(min[0]+", "+min[1]);
+        System.out.println(min[0] + ", " + min[1]);
     }
 }
--- prog/test/Kakao_2020_4.java
+++ prog/test/Kakao_2020_4.java
@@ -2,6 +2,5 @@ package prog;
 
 public class Kakao_2020_4 {
     public static void main(String[] args) {
-
     }
 }
