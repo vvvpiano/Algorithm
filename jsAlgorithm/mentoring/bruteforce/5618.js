70956d342d7c6af565a25a0354e643687fc07472
vvvpiano <vvvpiano60@gmail.com>
2021-07-19 19:08:06 +0900

    백준 5618

---
+++ jsAlgorithm/mentoring/bruteforce/5618.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const input = fs.readFileSync("../.././test.txt").toString().trim().split('\n');
+const numbers = input[1].split(' ').map(Number);
+
+// const solution = numbers => {
+//     const gcd = calculate_gcd(numbers);
+//     print_cd(gcd);
+// }
+
+// const calculate_gcd = numbers => { // gcd = greatest common denominator
+//     const min = Math.min(...numbers);
+//     for (let i = min; i > 1; i--) {
+//         let remain = numbers.map(e => e % i);
+//         let remainSum = remain.reduce((acc, cur) => acc + cur);
+//         if (remainSum === 0)
+//             return i;
+//     }
+//     return 1;
+// }
+
+// const print_cd = gcd => { // cd = common denominator
+//     for (let i = 1; i <= gcd; i++) {
+//         if (gcd % i === 0)
+//             console.log(i);
+//     }
+// }
+
+const solution = numbers => {
+    const min = Math.min(...numbers)
+    for (let i = 1; i <= min; i++) {
+        if (is_denominator(numbers, i))
+            console.log(i);
+    }
+}
+
+const is_denominator = (numbers, deno) => {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % deno != 0)
+            return false;
+    }
+    return true;
+}
+
+solution(numbers);
